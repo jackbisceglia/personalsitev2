32d92743eae39bfbc8f82ba172f993f235852247
jack <jackbisceglia2000@gmail.com>
2020-07-15 17:38:56 -0400

    Resume path updated

--- src/components/iconBar.js
+++ src/components/iconBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../global.css'
-import resume from './static/resume.pdf'
+import resume from '../images/JackBResumeV1.pdf'
 
 export default function IconBar(props) {
 
